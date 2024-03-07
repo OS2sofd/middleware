65c4c81133008cbf69bd5b971635cce0986818e9
Brian Graversen <bsg@digital-identity.dk>
2024-03-07 13:55:18 +0100

    signaturcentral is dead

--- sofd-core-signaturcentral-integration/src/main/resources/db/migration/V1_0__municipalities.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE municipality (
-  id                              BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name                            VARCHAR(255) NOT NULL,
-  password                        VARCHAR(255) NOT NULL,
-
-  sofd_url                        VARCHAR(255) NOT NULL,
-  sofd_api_key                    VARCHAR(36) NOT NULL
-);
